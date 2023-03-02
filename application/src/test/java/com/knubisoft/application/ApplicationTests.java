7315a2534ac8bbc797b35269e599b6c4673bbba9
Vladyslav Kolesnyk <v.kolesnyk@knubisoft.com>
2023-03-03 01:01:28 +0200

    init commit

---
+++ application/src/test/java/com/knubisoft/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knubisoft.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
