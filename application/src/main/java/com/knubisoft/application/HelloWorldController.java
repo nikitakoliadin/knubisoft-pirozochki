b9225c007a8a188764e726c6c0400c39a92ea957
Vladyslav Kolesnyk <v.kolesnyk@knubisoft.com>
2023-03-23 11:55:03 +0200

    logger added, config moved to github repo

--- application/src/main/java/com/knubisoft/application/HelloWorldController.java
+++ application/src/main/java/com/knubisoft/application/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.knubisoft.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,16 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
     @Value("${value}")
     String value;
 
     @RequestMapping("/hello")
     public String helloWorld() {
+        logger.info("Now we are at controller: {}", HelloWorldController.class.getSimpleName());
         return "Hello World";
     }
 
     @RequestMapping("/value")
     public String value() {
+        logger.info("Now we are at controller: {}", HelloWorldController.class.getSimpleName());
         return value;
     }
 }
