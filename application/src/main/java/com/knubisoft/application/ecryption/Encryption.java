ba0b2804ead3d676c1e72ccf18ac47b34b7a4fe3
Shumskyi Mykyta <n.shumsky@knubisoft.com>
2023-04-16 23:10:16 +0300

    moved bouncycastle version to pom file properties; methods encryptData() and decryptData now throws exception if one of the arguments is null;
    both encryptData() and decrypt data now accepts two byte arrays instead of X509Certificate and PrivateKey

--- application/src/main/java/com/knubisoft/application/ecryption/Encryption.java
+++ application/src/main/java/com/knubisoft/application/ecryption/Encryption.java
@@ -1,10 +1,7 @@
 package com.knubisoft.application.ecryption;
 
-import java.security.PrivateKey;
-import java.security.cert.X509Certificate;
-
 public interface Encryption {
 
-    byte[] encryptData(byte[] rawData, X509Certificate certificate);
-    byte[] decryptData(byte[] rawData, PrivateKey decryptionKey);
+    byte[] encryptData(byte[] rawData, byte[] certificate);
+    byte[] decryptData(byte[] rawData, byte[] decryptionKey);
 }
--- application/src/main/java/com/knubisoft/application/ecryption/impl/EncryptionImpl.java
+++ application/src/main/java/com/knubisoft/application/ecryption/impl/EncryptionImpl.java
@@ -13,42 +13,57 @@ import org.bouncycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
 import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
 import org.bouncycastle.cms.jcajce.JceKeyTransRecipient;
 import org.bouncycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.OutputEncryptor;
 
+import java.io.ByteArrayInputStream;
+import java.security.KeyFactory;
 import java.security.PrivateKey;
+import java.security.Security;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Collection;
 
-import static java.util.Objects.nonNull;
+import static java.util.Objects.isNull;
 
 public class EncryptionImpl implements Encryption {
 
     @Override
     @SneakyThrows
-    public byte[] encryptData(byte[] data, X509Certificate encryptionCertificate) {
-        if (nonNull(data) && nonNull(encryptionCertificate)) {
-            CMSEnvelopedDataGenerator cmsEnvelopedDataGenerator = new CMSEnvelopedDataGenerator();
-            JceKeyTransRecipientInfoGenerator jceKey = new JceKeyTransRecipientInfoGenerator(encryptionCertificate);
-            cmsEnvelopedDataGenerator.addRecipientInfoGenerator(jceKey);
-            CMSTypedData msg = new CMSProcessableByteArray(data);
-            OutputEncryptor encryptor = new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC)
-                    .setProvider("BC").build();
-            CMSEnvelopedData cmsEnvelopedData = cmsEnvelopedDataGenerator.generate(msg, encryptor);
-            return cmsEnvelopedData.getEncoded();
+    public byte[] encryptData(byte[] data, byte[] encryptionCertificateData) {
+        if (isNull(data) || isNull(encryptionCertificateData)) {
+            throw new RuntimeException("No data to encrypt");
         }
-        return data;
+        Security.addProvider(new BouncyCastleProvider());
+        X509Certificate encryptionCertificate = (X509Certificate) CertificateFactory.getInstance("X.509")
+                .generateCertificate(new ByteArrayInputStream(encryptionCertificateData));
+        CMSEnvelopedDataGenerator cmsEnvelopedDataGenerator = new CMSEnvelopedDataGenerator();
+        JceKeyTransRecipientInfoGenerator jceKey = new JceKeyTransRecipientInfoGenerator(encryptionCertificate);
+        cmsEnvelopedDataGenerator.addRecipientInfoGenerator(jceKey);
+        CMSTypedData msg = new CMSProcessableByteArray(data);
+        OutputEncryptor encryptor = new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC)
+                .setProvider("BC").build();
+        CMSEnvelopedData cmsEnvelopedData = cmsEnvelopedDataGenerator.generate(msg, encryptor);
+        return cmsEnvelopedData.getEncoded();
     }
+
 
     @Override
     @SneakyThrows
-    public byte[] decryptData(byte[] encryptedData, PrivateKey decryptionKey) {
-        if (nonNull(encryptedData) && nonNull(decryptionKey)) {
-            CMSEnvelopedData envelopedData = new CMSEnvelopedData(encryptedData);
-            Collection<RecipientInformation> recipients = envelopedData.getRecipientInfos().getRecipients();
-            KeyTransRecipientInformation recipientInfo = (KeyTransRecipientInformation) recipients.iterator().next();
-            JceKeyTransRecipient recipient = new JceKeyTransEnvelopedRecipient(decryptionKey);
-            return recipientInfo.getContent(recipient);
+    public byte[] decryptData(byte[] encryptedData, byte[] decryptionKeyData) {
+        if (isNull(encryptedData) || isNull(decryptionKeyData)) {
+            throw new RuntimeException("No data to decrypt");
         }
-        return encryptedData;
+        Security.addProvider(new BouncyCastleProvider());
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decryptionKeyData);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey decryptionKey = keyFactory.generatePrivate(keySpec);
+        CMSEnvelopedData envelopedData = new CMSEnvelopedData(encryptedData);
+        Collection<RecipientInformation> recipients = envelopedData.getRecipientInfos().getRecipients();
+        KeyTransRecipientInformation recipientInfo = (KeyTransRecipientInformation) recipients.iterator().next();
+        JceKeyTransRecipient recipient = new JceKeyTransEnvelopedRecipient(decryptionKey);
+        return recipientInfo.getContent(recipient);
     }
+
 }
