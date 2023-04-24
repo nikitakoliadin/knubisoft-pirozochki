8232963e8327ce2e109cb2f64faa4689ea734200
VadymKostenko <v.kostenko@knubisoft.com>
2023-04-24 18:07:42 +0300

    Fix test and clean up code

--- application/src/test/java/com/knubisoft/application/ApplicationTests.java
+++ application/src/test/java/com/knubisoft/application/ApplicationTests.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.nio.file.Files;
@@ -17,34 +18,42 @@ class ApplicationTests {
 
     @Autowired
     private Encryption encryption;
+    @Value("${keystore.password}")
+    private char[] keystorePassword;
+    @Value("${key.password}")
+    private char[] keyPassword;
+    private static final String original_data = "Hello, world!";
+    private static final String certificateData_path = "src/main/resources/test.cer";
+    private static final String privateKey_path = "src/main/resources/test.p12";
+    private static final String alias = "baeldung";
+    private static final String keyStoreInstance = "PKCS12";
+
     @Test
-    void contextLoads() {
+    @SneakyThrows
+    public void testEncryptData() {
+        byte[] certificateData = Files.readAllBytes(Paths.get(certificateData_path));
+        byte[] encryptedData = encryption.encryptData(original_data.getBytes(), certificateData);
+        Assertions.assertNotNull(encryptedData);
+        Assertions.assertNotEquals(original_data.getBytes(), encryptedData);
     }
 
     @Test
     @SneakyThrows
-    public void testEncryptAndDecrypt() {
-        // Read the certificate and private key from files
-        byte[] certificateData = Files.readAllBytes(Paths.get("src/main/resources/test.cer"));
+    public void decryptDate() {
+        byte[] certificateData = Files.readAllBytes(Paths.get(certificateData_path));
         byte[] privateKeyData = getPrivateKey();
-        String originalData = "Hello, world!";
-        byte[] originalDataBytes = originalData.getBytes();
-        // Encrypt the data
-        byte[] encryptedData = encryption.encryptData(originalDataBytes, certificateData);
-        // Decrypt the data
+        byte[] encryptedData = encryption.encryptData(original_data.getBytes(), certificateData);
         byte[] decryptedDataBytes = encryption.decryptData(encryptedData, privateKeyData);
         String decryptedData = new String(decryptedDataBytes);
-        // Check that the decrypted data is the same as the original data
-        Assertions.assertEquals(originalData, decryptedData);
+        Assertions.assertNotNull(decryptedData);
+        Assertions.assertEquals(original_data, decryptedData);
     }
 
-    private byte[] getPrivateKey() throws Exception {
-        char[] keystorePassword = "test".toCharArray();
-        char[] keyPassword = "test".toCharArray();
-        KeyStore keystore = KeyStore.getInstance("PKCS12");
-        keystore.load(Files.newInputStream(Paths.get("src/main/resources/test.p12")),
-                keystorePassword);
-        PrivateKey key = (PrivateKey) keystore.getKey("baeldung", keyPassword);
+    @SneakyThrows
+    private byte[] getPrivateKey() {
+        KeyStore keystore = KeyStore.getInstance(keyStoreInstance);
+        keystore.load(Files.newInputStream(Paths.get(privateKey_path)), keystorePassword);
+        PrivateKey key = (PrivateKey) keystore.getKey(alias, keyPassword);
         return key.getEncoded();
     }
 }
