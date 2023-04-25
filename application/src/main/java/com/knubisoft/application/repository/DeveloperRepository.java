47ab8e67bd17ae8a27b96575db898e5f2d702162
kolesnykv <v.kolesn1k369@gmail.com>
2023-04-25 13:50:25 +0300

    version moved to properties, added @repository annotation

--- application/src/main/java/com/knubisoft/application/repository/DeveloperRepository.java
+++ application/src/main/java/com/knubisoft/application/repository/DeveloperRepository.java
@@ -2,6 +2,8 @@ package com.knubisoft.application.repository;
 
 import com.knubisoft.application.model.Developer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeveloperRepository extends MongoRepository<Developer, Integer> {
 }
