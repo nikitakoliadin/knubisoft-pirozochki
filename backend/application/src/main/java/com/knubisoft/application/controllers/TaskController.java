5d7c3d23b35bcab03639e5cb4fafb63a3dfc95ab
Kostenko Vadym <v.kostenko@knubisoft.com>
2023-06-15 12:27:03 +0300

    Fix

--- backend/application/src/main/java/com/knubisoft/application/controllers/TaskController.java
+++ backend/application/src/main/java/com/knubisoft/application/controllers/TaskController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequiredArgsConstructor
--- backend/application/src/main/java/com/knubisoft/application/util/FireCpuLoadUtil.java
+++ backend/application/src/main/java/com/knubisoft/application/util/FireCpuLoadUtil.java
@@ -1,6 +1,5 @@
 package com.knubisoft.application.util;
 
-import com.knubisoft.application.model.Task;
 import com.sun.management.OperatingSystemMXBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
