f02ea4c7d558eab81f9d779a0d1af71774777c1c
Kostenko Vadym <v.kostenko@knubisoft.com>
2023-06-15 12:12:10 +0300

    Fix

--- backend/application/src/main/java/com/knubisoft/application/util/FireCpuLoadUtil.java
+++ backend/application/src/main/java/com/knubisoft/application/util/FireCpuLoadUtil.java
@@ -1,5 +1,6 @@
 package com.knubisoft.application.util;
 
+import com.knubisoft.application.model.Task;
 import com.sun.management.OperatingSystemMXBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
