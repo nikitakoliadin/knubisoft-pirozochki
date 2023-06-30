97de22ea3a4a96ef2d810e14817fb851e8abfd58
kolesnykv <v.kolesn1k369@gmail.com>
2023-06-30 22:45:56 +0300

    removed rabbit consumer from application module

--- backend/application/src/main/java/com/knubisoft/application/controllers/MessageController.java
+++ backend/application/src/main/java/com/knubisoft/application/controllers/MessageController.java
@@ -1,11 +1,7 @@
 package com.knubisoft.application.controllers;
 
 import com.knubisoft.application.rabbitmq.RabbitMessageSender;
-import com.knubisoft.application.rabbitmq.Receiver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     private final RabbitMessageSender rabbitMessageSender;
-    private final Receiver receiver;
 
     @PostMapping("/send")
     public void sendMessage() {
@@ -28,9 +23,5 @@ public class MessageController {
     public void sendCustomMessage(@RequestBody String customMessage) {
         rabbitMessageSender.sendCustomMessage(customMessage);
     }
-    @GetMapping("/get")
-    public String receiveMessage() throws InterruptedException {
-        receiver.getLatch().await();
-        return receiver.getLastMessage();
-    }
+
 }
