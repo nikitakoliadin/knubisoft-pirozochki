b17ba030b87137e1956f247513fe94f71ba48f4c
Shumskyi Mykyta <n.shumsky@knubisoft.com>
2023-08-13 13:30:30 +0300

    - footer layout

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+
+createApp(App).mount('#app');
+App.use(PrimeVue);
