b791ef4aeaab27f9a65b5b2d3f2fbfa83a2c42ee
Kostenko Vadym <v.kostenko@knubisoft.com>
2023-11-18 01:13:00 +0200

    +

---
+++ backend/application/src/main/java/com/knubisoft/application/openAi/CodeMirrorResponse.java
@@ -0,0 +1,12 @@
+package com.knubisoft.application.openAi;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Setter
+@Getter
+public class CodeMirrorResponse {
+    private String code;
+}
